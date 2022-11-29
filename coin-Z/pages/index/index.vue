d48311f68604ca4684c285686ce9c672da7e7e84
3240232 <liusiqi@pin-dao.cn>
2022-11-30 00:32:59 +0800

    转换时间格式

--- coin-Z/pages/index/index.vue
+++ coin-Z/pages/index/index.vue
@@ -14,53 +14,55 @@
 				<button @click="seach" class="jump-next" size="mini" type="primary">搜索</button>
 			<!-- </navigator> -->
 		</view>
-		<uni-table style="height: 30%;" ref="table" border stripe type="selection" @selection-change="selectionChange">
-			<uni-tr>
-				<uni-th width="150" align="center">url</uni-th>
-				<uni-th width="150" align="center">商品图片</uni-th>
-				<uni-th width="150" align="center">ID</uni-th>
-				<uni-th align="center">商品名称</uni-th>
-				<uni-th align="center">价格</uni-th>
-				<uni-th align="center">标题</uni-th>
-				<uni-th align="center">描述</uni-th>
-				<uni-th align="center">所属分类</uni-th>
-				<uni-th align="center">新建时间</uni-th>
-				<uni-th align="center">更新时间</uni-th>
-				<uni-th align="center">商品状态</uni-th>
-				<uni-th width="204" align="center">设置</uni-th>
-			</uni-tr>
-			<uni-tr v-for="(item, index) in tableData" :key="index">
-				<uni-td align="center">{{ item.url }}</uni-td>
-				<uni-td align="center">
-					<image style="width: 60px;height: 60px;" :src="item.mainUrl" mode=""></image>
-				</uni-td>
-				<uni-td align="center">{{ item.spuCode }}</uni-td>
-				<uni-td align="center">{{ item.spuName }}</uni-td>
-				<uni-td align="center">{{ item.price }}</uni-td>
-				<uni-td align="center">{{ item.title }}</uni-td>
-				<uni-td align="center">{{ item.desc }}</uni-td>
-				<uni-td align="center">{{ item.type }}</uni-td>
-				<uni-td align="center">{{ item.create_time }}</uni-td>
-				<uni-td align="center">{{ item.update_time }}</uni-td>
-				<uni-td align="center">{{ item.status }}</uni-td>
-				<uni-td align="center">
-					<view class="uni-group">
-						<button class="uni-button" size="mini" type="primary" @click="edit(item.spuCode)">修改</button>
-						<button class="uni-button" size="mini" type="warn" @click="deleteBut(item.spuCode)">删除</button>
+		<view class="content">
+			<uni-table style="height: 475px;" ref="table" border stripe type="selection" @selection-change="selectionChange">
+					<uni-tr>
+						<uni-th width="150" align="center">url</uni-th>
+						<uni-th width="150" align="center">商品图片</uni-th>
+						<uni-th width="150" align="center">ID</uni-th>
+						<uni-th align="center">商品名称</uni-th>
+						<uni-th align="center">价格</uni-th>
+						<uni-th align="center">标题</uni-th>
+						<uni-th align="center">描述</uni-th>
+						<uni-th align="center">所属分类</uni-th>
+						<uni-th align="center">新建时间</uni-th>
+						<uni-th align="center">更新时间</uni-th>
+						<uni-th align="center">商品状态</uni-th>
+						<uni-th width="204" align="center">设置</uni-th>
+					</uni-tr>
+					<uni-tr v-for="(item, index) in tableData" :key="index">
+						<uni-td align="center">{{ item.url }}</uni-td>
+						<uni-td align="center">
+							<image style="width: 60px;height: 60px;" :src="item.mainUrl" mode=""></image>
+						</uni-td>
+						<uni-td align="center">{{ item.spuCode }}</uni-td>
+						<uni-td align="center">{{ item.spuName }}</uni-td>
+						<uni-td align="center">{{ item.price }}</uni-td>
+						<uni-td align="center">{{ item.title }}</uni-td>
+						<uni-td align="center">{{ item.desc }}</uni-td>
+						<uni-td align="center">{{ item.type }}</uni-td>
+						<uni-td align="center">{{ item.create_time | formatDate }}</uni-td>
+						<uni-td align="center">{{ item.update_time | formatDate }}</uni-td>
+						<uni-td align="center">{{ item.status }}</uni-td>
+						<uni-td align="center">
+							<view class="uni-group">
+								<button class="uni-button" size="mini" type="primary" @click="edit(item.spuCode)">修改</button>
+								<button class="uni-button" size="mini" type="warn" @click="deleteBut(item.spuCode)">删除</button>
+							</view>
+						</uni-td>
+					</uni-tr>
+			</uni-table>
+			<view class="footer uni-pagination-box">
+				<uni-pagination 
+					show-icon 
+					:page-size="pageSize" 
+					:current="pageCurrent" 
+					:total="total" 	
+					@change="change" />
+					<view>
+						<text class="example-info">当前页：{{ pageCurrent }}，数据总量：{{ total }}条，每页数据：{{ pageSize }}</text>
 					</view>
-				</uni-td>
-			</uni-tr>
-		</uni-table>
-		<view class="uni-pagination-box">
-			<uni-pagination 
-				show-icon 
-				:page-size="pageSize" 
-				:current="pageCurrent" 
-				:total="total" 	
-				@change="change" />
-				<view>
-					<text class="example-info">当前页：{{ pageCurrent }}，数据总量：{{ total }}条，每页数据：{{ pageSize }}</text>
-				</view>
+			</view>
 		</view>
 	</view>
 </template>
@@ -79,25 +81,65 @@
 				tableData: [],
 			}
 		},
+		onLoad() {
+				this.selectedIndexs = []
+				this.seach(1)
+			},
 		mounted() {
 			this.seach()
 		},
+		filters: {
+			formatDate: function (cellValue) {
+				console.log("cellValue",cellValue);
+				if (cellValue == null || cellValue == "") return "";
+				  var date = new Date(cellValue)
+				  var year = date.getFullYear()
+				  var month = date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1
+				  var day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate()
+				  var hours = date.getHours() < 10 ? '0' + date.getHours() : date.getHours()
+				  var minutes = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()
+				  var seconds = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds()
+				  return year + '-' + month + '-' + day + ' ' + hours + ':' + minutes + ':' + seconds
+				// console.log("value",value);
+				// var date = new Date(value);//时间戳为10位需*1000，时间戳为13位的话不需乘1000
+				// console.log("date",date);
+				// var Y = date.getFullYear() + '-';
+				// var M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
+				// var D = date.getDate() + '-';
+				// var h = date.getHours() + ':';
+				// var m = date.getMinutes() + ':';
+				// var s = date.getSeconds();
+				// return Y+M+D+h+m+s;
+			}
+		},
 		methods: {
-			seach(){
+			// 分页触发
+			change(e) {
+				this.$refs.table.clearSelection()
+				this.selectedIndexs.length = 0
+				this.seach(e.current)
+			},
+			seach(pageCurrent, value = ''){
+				console.log("pageCurrent",pageCurrent);
 				let params = {
 				          // "phone":this.userphone,
 				          // "name":this.username
 				
 				    }
+					this.pageCurrent = pageCurrent
 				    uni.request({
-				          url: `${this.$baseUrl}/spu/findAll`,
-				          method: 'GET',
-				          success: (res)=>{
+						// pageSize: this.pageSize,
+						// pageCurrent: pageCurrent,
+						// value: value,
+						  url: `${this.$baseUrl}/spu/findAll`,
+						  method: 'GET',
+						  success: (res)=>{
 							  console.log("res",res)
 							  this.tableData = res.data.data
 							  this.total = res.data.total
+							  console.log("this.tableData",this.tableData);
 						  },
-				          fail: (err)=>{}
+						  fail: (err)=>{}
 				    })  
 			},
 			jumpSecondPage(){
@@ -146,4 +188,27 @@
 .view{
 	margin:10px
 }
+.content{
+		/* width: 750rpx; */
+		font-family: PingFang SC-Regular, PingFang SC;
+	}
+	/* 表格标题固定定位 */
+	.table_head{
+		position: fixed;
+		margin-top: 50px;
+		/* top: 0; */
+		left: 0;
+	}
+	.table_body{
+		margin-top: 100px;
+	}
+	/* 底部 */
+	.footer{
+		position: fixed;
+		left: 0rpx;
+		bottom: 0rpx;
+		width: 100%;
+		height: 126rpx;
+		background: #F5F5F5;
+	}
 </style>
